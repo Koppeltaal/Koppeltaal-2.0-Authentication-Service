111bec66971ff1e0e8ac6c78d20d449df12debce
Joris Scharp <joris@headease.nl>
2022-12-08 09:21:03 +0100

    Reverting model.py, if the database is initialized by Java, this application can connect. It should just not initialize it.

--- application/oauth_server/model.py
+++ application/oauth_server/model.py
@@ -14,7 +14,7 @@ from application.oauth_server.guid import GUID
 
 
 class Oauth2Session(db.Model):
-    id = db.Column(db.String(36), primary_key=True, default=uuid4, unique=True)
+    id = db.Column(GUID(), primary_key=True, default=uuid4, unique=True)
 
     type = db.Column(db.String(80))  # alter table oauth2_session add column type VARCHAR(80) default 'smart_backend'
     scope = db.Column(db.Text())
@@ -35,14 +35,14 @@ class Oauth2Session(db.Model):
 
 
 class Oauth2Token(db.Model):
-    id = db.Column(db.String(36), primary_key=True, default=uuid4, unique=True)
+    id = db.Column(GUID(), primary_key=True, default=uuid4, unique=True)
     client_id = db.Column(db.String(80))
     access_token = db.Column(db.Text())
     refresh_token = db.Column(db.Text())
     id_token = db.Column(db.String(2048))
     scope = db.Column(db.Text())
     subject = db.Column(db.String(128))
-    session_id = db.Column(db.String(36), db.ForeignKey(Oauth2Session.id))
+    session_id = db.Column(GUID(), db.ForeignKey(Oauth2Session.id))
 
     def to_json(self):
         return {'client_id': self.client_id,
@@ -81,7 +81,7 @@ class Role(db.Model):
       PRIMARY KEY (`id`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
     """
-    id = db.Column(db.String(36), primary_key=True, default=uuid4, unique=True)
+    id = db.Column(GUID(), primary_key=True, default=uuid4, unique=True)
     created_by = db.Column(db.String(255))
     created_on = db.Column(db.DateTime())
     name = db.Column(db.String(255))
@@ -108,14 +108,14 @@ class SmartService(db.Model):
       CONSTRAINT `FKcosi5jmx6d18vmwqhv2h3gmr0` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
     """
-    id = db.Column(db.String(36), primary_key=True, default=uuid4, unique=True)
+    id = db.Column(GUID(), primary_key=True, default=uuid4, unique=True)
     created_by = db.Column(db.String(255))
     created_on = db.Column(db.DateTime())
     client_id = db.Column(db.String(255))
     jwks_endpoint = db.Column(db.String(255))
-    status = db.Column(db.String(255))
+    status = db.Column(db.Enum(SmartServiceStatus))
     public_key = db.Column(db.String(255))
-    role_id = db.Column(db.String(36), ForeignKey(Role.id))
+    role_id = db.Column(GUID(), ForeignKey(Role.id))
     role = relationship("Role")
     name = db.Column(db.String(255))
     fhir_store_device_id = db.Column(db.String(255))
@@ -136,13 +136,13 @@ class Permission(db.Model):
       CONSTRAINT `FKrvhjnns4bvlh4m1n97vb7vbar` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
     """
-    id = db.Column(db.String(36), primary_key=True, default=uuid4, unique=True)
+    id = db.Column(GUID(), primary_key=True, default=uuid4, unique=True)
     created_by = db.Column(db.String(255))
     created_on = db.Column(db.DateTime())
-    operation = db.Column(db.String(255))
+    operation = db.Column(db.Enum(CrudOperation))
     resource_type = db.Column(db.String(255))
-    scope = db.Column(db.String(255))
-    role_id = db.Column(db.String(36), ForeignKey("role.id"))
+    scope = db.Column(db.Enum(PermissionScope))
+    role_id = db.Column(GUID(), ForeignKey("role.id"))
     role = relationship("Role")
 
 
@@ -157,5 +157,5 @@ class PermissionServiceGrant(db.Model):
       CONSTRAINT `FKdc1aains9omcxwoulinqyvr7j` FOREIGN KEY (`smart_service_id`) REFERENCES `smart_service` (`id`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
     """
-    permission_id = db.Column(db.String(36), ForeignKey('permission.id'), primary_key=True)
-    smart_service_id = db.Column(db.String(36), ForeignKey('smart_service.id'), primary_key=True)
+    permission_id = db.Column(GUID(), ForeignKey('permission.id'), primary_key=True)
+    smart_service_id = db.Column(GUID(), ForeignKey('smart_service.id'), primary_key=True)
