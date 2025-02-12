68bada90dd709641e1731508a1c91ce8572e7507
Roland Groen <roland@headease.nl>
2025-02-12 14:00:08 +0100

    Fix JWKS client issue with VECOZO by updating User-Agent
    Addressed a compatibility issue where VECOZO rejects requests with the default `Python-urllib/3.8` User-Agent. Added a fallback to use `Mozilla/5.0` User-Agent for retrying requests, resolving status code errors in the 4xx range.

--- application/oauth_server/jwks_client.py
+++ application/oauth_server/jwks_client.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+import requests
 from jwt import PyJWKClient, PyJWKClientError
 from requests_cache import CachedSession
 
@@ -20,4 +21,9 @@ class CacheHeaderPyJWKClient(PyJWKClient):
                 except ValueError as e:
                     raise PyJWKClientError(f'Fail parse data from the url, err: "{e}"')
             else:
+                ## Hack fix for VECOZO not accepting the `Python-urllib/3.8` User-Agent
+                if response.status_code >= 400 < 500:
+                    resp = requests.get(self.uri, headers={"User-Agent": "Mozilla/5.0"})
+                    if resp.status_code == 200:
+                        return resp.json()
                 raise PyJWKClientError(f'Fail to fetch data from the url, status code: "{response.status_code}"')
