6df206e77d97c6af872105796beb3c4b18e701d6
Roland Groen <roland@headease.nl>
2025-02-13 10:48:02 +0100

    Add logging for fallback User-Agent in CacheHeaderPyJWKClient
    Introduce a warning log when falling back to the "Mozilla/5.0" User-Agent to handle specific HTTP errors. This improves debugging by making the fallback behavior more transparent in the logs.

--- application/oauth_server/jwks_client.py
+++ application/oauth_server/jwks_client.py
@@ -1,9 +1,12 @@
 from typing import Any
 
 import requests
+import logging
 from jwt import PyJWKClient, PyJWKClientError
 from requests_cache import CachedSession
 
+
+logger = logging.getLogger('CacheHeaderPyJWKClient')
 
 class CacheHeaderPyJWKClient(PyJWKClient):
     def __init__(self,
@@ -23,7 +26,9 @@ class CacheHeaderPyJWKClient(PyJWKClient):
             else:
                 ## Hack fix for VECOZO not accepting the `Python-urllib/3.8` User-Agent
                 if response.status_code >= 400 < 500:
-                    resp = requests.get(self.uri, headers={"User-Agent": "Mozilla/5.0"})
+                    user_agent = "Mozilla/5.0"
+                    logger.warning("Falling back to User-Agent: " + user_agent)
+                    resp = requests.get(self.uri, headers={"User-Agent": user_agent})
                     if resp.status_code == 200:
                         return resp.json()
                 raise PyJWKClientError(f'Fail to fetch data from the url, status code: "{response.status_code}"')
